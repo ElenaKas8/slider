74c8619b7ac6a14f25abfaeb4047b0585a7c22d2
Elena <149805926+ElenaKas8@users.noreply.github.com>
2024-03-14 20:17:36 +0100

    round-btn

--- Slider/slider.js
+++ Slider/slider.js
@@ -9,7 +9,7 @@ const images = [
     'https://galerey-room.ru/images/0_69122_3f69c4d2_orig.jpeg',
     'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Altdorfer-Donau.jpg/220px-Altdorfer-Donau.jpg',
     'https://oir.mobi/uploads/posts/2021-06/1622803069_29-oir_mobi-p-peizazh-dlya-nachinayushchikh-priroda-kras-30.jpg',
-    'https://arthive.net/res/media/img/oy1000/work/577/610144@2x.jpg'
+    'https://arthive.net/res/media/img/oy1000/work/577/610144@2x.jpg',
 ]
 
 for (let elem of images){
@@ -30,6 +30,7 @@ rigtnBtn.onclick = () => {
         slider_index = 0
         cards_container.style.left = '0px'
     }
+    changeActiveBtn()
 }
 
 leftBtn.onclick = () => {
@@ -39,4 +40,35 @@ leftBtn.onclick = () => {
         slider_index = images.length - 1
     }
     cards_container.style.left = `${-1 * slider_index * card_width}px`
+    changeActiveBtn()
 }
+
+// -----------------------------
+// Создание кнопок
+
+const slider_btns = document.querySelector('.slider_btns')
+
+for (let i = 0; i < images.length; i++){
+    const round_btn = document.createElement('button')
+    round_btn.className = 'round_btn'
+    slider_btns.append(round_btn)
+
+    round_btn.onclick = () => {
+        slider_index = i
+        cards_container.style.left = `${-1 * slider_index * card_width}px`
+        changeActiveBtn()
+    }
+}
+
+function changeActiveBtn(){
+    const all_btns = document.querySelectorAll('.round_btn')
+    all_btns.forEach((elem, index) => {
+        if (slider_index === index){
+            elem.classList.add('active')
+        } else {
+            elem.classList.remove('active')
+        }
+    })
+}
+
+changeActiveBtn()
